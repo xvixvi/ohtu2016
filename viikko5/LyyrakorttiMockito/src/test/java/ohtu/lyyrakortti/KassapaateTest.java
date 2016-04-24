b127a6aa622e47e92e2027701a68f99b1d534f94
xvixvi <vili.hatonen@helsinki.fi>
2016-04-24 23:40:36 +0300

    asdf

--- viikko5/LyyrakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
+++ viikko5/LyyrakorttiMockito/src/test/java/ohtu/lyyrakortti/KassapaateTest.java
@@ -45,4 +45,18 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
+    
+    @Test
+    public void lataaJosPos() {
+        kassa.lataa(kortti, 5);
+        
+        verify(kortti, times(1)).lataa(eq(5));
+    }
+    
+    @Test
+    public void lataaJosNeg() {
+        kassa.lataa(kortti, -666);
+        
+        verify(kortti, times(0)).lataa(anyInt());
+    }
 }
