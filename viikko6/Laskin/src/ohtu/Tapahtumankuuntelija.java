83dbc34995b9c58d63f3ea22fbab644b2ab8675d
xvixvi <vili.hatonen@helsinki.fi>
2016-05-02 22:31:38 +0300

    ASD

--- viikko6/Laskin/src/ohtu/Tapahtumankuuntelija.java
+++ viikko6/Laskin/src/ohtu/Tapahtumankuuntelija.java
@@ -2,10 +2,12 @@ package ohtu;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 import javax.swing.JButton;
 import javax.swing.JTextField;
- 
+
 public class Tapahtumankuuntelija implements ActionListener {
+
     private JButton plus;
     private JButton miinus;
     private JButton nollaa;
@@ -13,7 +15,8 @@ public class Tapahtumankuuntelija implements ActionListener {
     private JTextField tuloskentta;
     private JTextField syotekentta;
     private Sovelluslogiikka sovellus;
- 
+    private HashMap<String, Komento> komennot;
+
     public Tapahtumankuuntelija(JButton plus, JButton miinus, JButton nollaa, JButton undo, JTextField tuloskentta, JTextField syotekentta) {
         this.plus = plus;
         this.miinus = miinus;
@@ -22,37 +25,91 @@ public class Tapahtumankuuntelija implements ActionListener {
         this.tuloskentta = tuloskentta;
         this.syotekentta = syotekentta;
         this.sovellus = new Sovelluslogiikka();
+        komennot = new HashMap<String, Komento>();
+        komennot.put("plus", new Plus(sovellus));
+        komennot.put("miinus", new Miinus(sovellus));
+        komennot.put("nollaa", new Nollaa(sovellus));
+        komennot.put("tuntematon", new Tuntematon(sovellus));
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent ae) {
         int arvo = 0;
- 
+
         try {
             arvo = Integer.parseInt(syotekentta.getText());
         } catch (Exception e) {
+            
         }
- 
-        if (ae.getSource() == plus) {
-            sovellus.plus(arvo);
-        } else if (ae.getSource() == miinus) {
-            sovellus.miinus(arvo);
-        } else if (ae.getSource() == nollaa) {
-            sovellus.nollaa();
-        } else {
-            System.out.println("undo pressed");
+        Komento komento = komennot.get(ae);
+        if (komento == null) {
+            komento = komennot.get("tuntematon");
         }
         
+        komento.suorita(arvo);
+
         int laskunTulos = sovellus.tulos();
-         
+
         syotekentta.setText("");
         tuloskentta.setText("" + laskunTulos);
-        if ( laskunTulos==0) {
-            nollaa.setEnabled(false);
-        } else {
-            nollaa.setEnabled(true);
-        }
+        nollaa.setEnabled(laskunTulos != 0);
         undo.setEnabled(true);
     }
- 
+
+    private static class Miinus implements Komento {
+
+        private final Sovelluslogiikka sovellus;
+
+        public Miinus(Sovelluslogiikka sovellus) {
+            this.sovellus = sovellus;
+        }
+
+        @Override
+        public void suorita(int arvo) {
+            sovellus.miinus(arvo);
+        }
+    }
+
+    private static class Nollaa implements Komento {
+
+        private final Sovelluslogiikka sovellus;
+
+        public Nollaa(Sovelluslogiikka sovellus) {
+            this.sovellus = sovellus;
+        }
+
+        @Override
+        public void suorita(int arvo) {
+            sovellus.nollaa();
+        }
+    }
+
+    private static class Tuntematon implements Komento {
+
+        private final Sovelluslogiikka sovellus;
+
+        public Tuntematon(Sovelluslogiikka sovellus) {
+            this.sovellus = sovellus;
+        }
+
+        @Override
+        public void suorita(int arvo) {
+            System.out.println("tuntematon komento");
+        }
+    }
+
+    private class Plus implements Komento {
+
+        private final Sovelluslogiikka sovellus;
+
+        public Plus(Sovelluslogiikka sovellus) {
+            this.sovellus = sovellus;
+        }
+
+        @Override
+        public void suorita(int arvo) {
+            sovellus.plus(arvo);
+        }
+    }
+
 }
